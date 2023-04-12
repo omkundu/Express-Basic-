60fcccf8dd44a60dec366cbb09e4930f57e4f749
omkundu <omkundu156@gmail.com>
2023-04-12 17:13:52 +0530

    express-second-updated

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 // console.log("Hello");
-
+const fs=require("fs");
 const express =require("express");
 
   const app=express()
+  app.use(express.json())
 
 app.get("/",(req,res)=>{
-    res.end("hello");
+    res.send("hello");
 })
 
-// app.post("/adddetails",(req,res)=>{
-//     res.end("accepted data")
-// })
+app.post("/adddata",(req,res)=>{
+    console.log(req.body)
+    res.end("accepted data")
+})
 
 
+app.post("/addStudent",(req,res)=>{
+    fs.writeFileSync("./students.txt",JSON.stringify(req.body),"utf-8")
+    res.send("accepted data");
+})
 
+app.get("/students",(req,res)=>{
+    const data=fs.readFileSync("./students.txt","utf-8")
+    res.send(data);
+})
 
   app.listen(7000,()=>{
     console.log("Listening on port 7000")
